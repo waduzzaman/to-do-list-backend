c7fdb4017f2b639add1043e3c3f805b76322f559
Waduzzaman <waduzzaman@gmail.com>
2023-03-19 04:44:52 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+/*
+Template to create a server
+
+// jshint esversion:6
+const express=require('express');
+const bodyParser=require('body-parser');
+
+const app=express();
+
+app.get('/', function(req, res){
+    res.send("hello")
+});
+
+app.listen( 3000, function(){
+    console.log('server is running on port 3000');
+});
+
+*/
+
+// jshint esversion:6
+const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
+const date=require(__dirname +'/date.js')
+
+const app = express();
+const items = [ "Buy Food", "Cook Food", "Eat Food", ];
+const workItems = [];
+
+app.set( 'view engine', 'ejs' );
+
+// use body parser
+app.use( bodyParser.urlencoded( { extended: true } ) )
+// Always need to pase this after app 
+// we use this ejs to create template 
+app.use( express.static( 'public' ) );
+
+
+app.get( '/', function ( req, res )
+{
+   const day=date.getDate();
+
+    res.render( "list", { listTitle: day, newListItems: items } );
+
+} );
+
+app.post( '/', function ( req, res )
+{
+    const item = req.body.newItem;
+    if ( req.body.list === "Work" )
+    {
+        workItems.push( item );
+        res.redirect('/work')
+
+    } else
+    {
+        items.push( item );
+        res.redirect( '/' );
+
+    }
+
+
+
+} );
+
+app.get( '/work', function ( req, res )
+{
+    res.render( 'list', { listTitle: 'Work List', newListItems: workItems } );
+
+} );
+
+app.post( '/work', function ( req, res )
+{
+    const item = req.body.newItem;
+    workItems.push();
+    res.redirect( '/work' );
+} );
+
+app.get('/about', function(req,res){
+    res.render('about');
+});
+
+app.listen( 3000, function ()
+{
+    console.log( 'server is running on port 3000' );
+} );
